39ab7c0c91fbb228fb9203b5834d580a7081962a
ashmanq <ashirq@outlook.com>
2020-06-06 08:52:03 +0100

    Updated node dependencies

--- src/App.js
+++ src/App.js
@@ -1,27 +1,57 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, CardSection, Card, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 
 class App extends Component {
+
+    state = { loggedIn: null };
+
     componentWillMount() {
         firebase.initializeApp({
-                apiKey: 'AIzaSyB_FuGdfA3R695myIZmhxbtDh7PLwWgfT4',
-                authDomain: 'auth-5d6a2.firebaseapp.com',
-                databaseURL: 'https://auth-5d6a2.firebaseio.com',
-                projectId: 'auth-5d6a2',
-                storageBucket: 'auth-5d6a2.appspot.com',
-                messagingSenderId: '718153650607',
-                appId: '1:718153650607:web:3f1afcdb16c80c79'
-        })
+            apiKey: 'AIzaSyB_FuGdfA3R695myIZmhxbtDh7PLwWgfT4',
+            authDomain: 'auth-5d6a2.firebaseapp.com',
+            databaseURL: 'https://auth-5d6a2.firebaseio.com',
+            projectId: 'auth-5d6a2',
+            storageBucket: 'auth-5d6a2.appspot.com',
+            messagingSenderId: '718153650607',
+            appId: '1:718153650607:web:3f1afcdb16c80c79'
+        });
+
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                this.setState({ loggedIn: true });
+            } else {
+                this.setState({ loggedIn: false });
+            }
+        });
     }
+
+    renderContent() {
+        switch (this.state.loggedIn) {
+            case true:
+                return (<Card>
+                    <CardSection>
+                        <Button onPress={() => firebase.auth().signOut()}>
+                            Log Out
+                        </Button>
+                    </CardSection>
+                </Card>);
+            case false:
+                return <LoginForm />;
+            default:
+                return <Spinner size='large' />;
+        };
+
+    }
+
     render() {
         return (
             <View>
                 <Header headerText="Authentication" />
-                <LoginForm />
+                {this.renderContent()}
             </View>
         );
     }
